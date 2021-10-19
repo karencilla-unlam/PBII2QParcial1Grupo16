b4d2ab7898589299442fafe932fb015bbe76248b
Emmanuel Orieta <emmanuel.orieta@gmail.com>
2021-10-18 22:42:20 -0300

    Se resuben los cambios por error en consola al momento de hacer el pull. Se modificaron metodos y se hizo un test que dio verde

--- test/Veterinaria/VeterinariaTest.java
+++ test/Veterinaria/VeterinariaTest.java
@@ -4,25 +4,24 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-	
-public class VeterinariaTest{
-		
+public class VeterinariaTest {
+
 	@Test
 	public void alIngresarUnAnimalSeCrea() {
 		Animal animal = new Animal();
-		//assertTrue(animal.pesar());
+		// assertTrue(animal.pesar());
 	}
 
 	@Test
 	public void TestQueUnLoroVuela() {
-		//Preparacion
-		Loro unLoro= new Loro();
-		Boolean valorEsperado=true;
+		// Preparacion
+		Loro unLoro = new Loro();
+		Boolean valorEsperado = true;
 		Boolean valorRecibido;
-		//Ejecucion
-		valorRecibido=unLoro.volar();
-		//Contrastacion
+		// Ejecucion
+		valorRecibido = unLoro.volar();
+		// Contrastacion
 		assertEquals(valorEsperado, valorRecibido);
-		
+
 	}
 }
